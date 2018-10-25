5261cc8c67efa232865623a83fde0c9a3abd9006
cheshirecats <curiouswall@gmail.com>
2018-10-26 01:42:49 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </div></div>
 <script> var $CURRENT_FILE_NAME = "<?php echo htmlentities(basename($_SERVER['PHP_SELF'])); ?>"; </script>
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script type="text/javascript" src="//cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
 <script type="text/javascript" src="gaq.js"></script>
 <script type="text/javascript" src="prettify.js"></script>
 <script type="text/javascript" src="script.js"></script>
